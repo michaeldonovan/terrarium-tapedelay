d584e4ec74f0c56bd57e11928a05877c4dada25a
Michael Donovan <michael@mdonovan.dev>
2022-12-12 19:21:51 -0600

    Add Constants.hpp, Params.hpp

---
+++ src/Constants.hpp
@@ -0,0 +1,52 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#include <daisy.h>
+
+constexpr auto DAISY_SR = daisy::SaiHandle::Config::SampleRate::SAI_48KHZ;
+
+constexpr float SampleRate()
+{
+    switch(DAISY_SR)
+    {
+        case daisy::SaiHandle::Config::SampleRate::SAI_96KHZ:
+            return 96000;
+            break;
+        case daisy::SaiHandle::Config::SampleRate::SAI_48KHZ:
+            return 48000;
+            break;
+        case daisy::SaiHandle::Config::SampleRate::SAI_32KHZ:
+            return 32000;
+            break;
+        case daisy::SaiHandle::Config::SampleRate::SAI_16KHZ:
+            return 16000;
+            break;
+        case daisy::SaiHandle::Config::SampleRate::SAI_8KHZ:
+            return 8000;
+            break;
+    }
+};
+
+constexpr auto HOLD_MS = 250;
+
+// delay params
+constexpr auto MIN_DELAY_SEC = 0.1f;
+constexpr auto MAX_DELAY_SEC = 1.f;
+constexpr size_t MIN_DELAY = SampleRate() * MIN_DELAY_SEC;
+constexpr size_t MAX_DELAY = SampleRate() * MAX_DELAY_SEC;
+
+constexpr auto DELAY_SMOOTH_MS = 500.f;
+
+// tape params
+constexpr auto WOW_FREQ = 0.4f;
+constexpr auto FLUTTER_FREQ = 25.f;  // try lower
+constexpr float WOW_MAX_AMP = .0092 * SampleRate();
+constexpr float FLUTTER_MAX_AMP = 0.0008 * SampleRate();
+
+constexpr float LOSS_LP_FC_MIN = 1000.f;
+constexpr float LOSS_LP_FC_MAX = 6000.f;
+
+constexpr float TAPE_DRIVE_DB_MIN = 6.f;
+constexpr float TAPE_DRIVE_DB_MAX = 14.f;
+
+#endif  // CONSTANTS_HPP
---
+++ src/Params.hpp
@@ -0,0 +1,14 @@
+#ifndef PARAMS_HPP
+#define PARAMS_HPP
+
+struct Params
+{
+    float time_active;
+    float time_knob;
+    float feedback;
+    float mix;
+    float age;
+    float stability;
+}
+
+#endif  // PARAMS_HPP
